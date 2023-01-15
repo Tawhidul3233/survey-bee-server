d820a72289c30aec28fdf30c46ecfbed48197709
oliullah <razasm29@gmail.com>
2023-01-15 15:09:57 +0600

    initial set up done and mongodb connected done

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const app = express();
+
+const port = process.env.PORT || "5000";
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://surveyBee:surveyBeeBeeSurvey@surveybee.idkf3x9.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+// run mongodb
+async function dbConnect() {
+  try {
+    await client.connect();
+    console.log("database connected");
+
+
+    // database collections here
+  } finally {
+  }
+}
+dbConnect().catch((err) => console.log(err));
+
+// test server endpoint
+app.get("/", (req, res) => {
+  res.json({
+    status: true,
+    message: " server is ready to use",
+  });
+});
+
+app.listen(port, () => {
+  console.log(` server is running on: ${port}`);
+});
